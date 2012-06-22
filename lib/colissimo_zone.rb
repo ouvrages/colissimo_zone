40c282b4e3fadc12927871a87c9572581895babd
Michael Witrant <michael@ouvrages-web.fr>
2012-06-22 17:59:24 +0200

    module code

--- lib/colissimo_zone.rb
+++ lib/colissimo_zone.rb
@@ -1,5 +1,18 @@
 require "colissimo_zone/version"
 
 module ColissimoZone
-  # Your code goes here...
+  class << self
+    def data_filename
+      File.expand_path('../data/colissimo_zones.yaml', __FILE__)
+    end
+    
+    def parse
+      require 'yaml'
+      YAML.load(File.read(data_filename))
+    end
+    
+    def all
+      parse
+    end
+  end
 end
